f57dd9e88910cda756a7daa9643b6821112609d6
theemadnes <alex.mattson@gmail.com>
2019-10-09 01:31:05 -0500

    add all this stuff

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.7-alpine
+
+#MAINTAINER Alex Mattson "alex.mattson@gmail.com"
+# test
+
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+COPY . /app
+
+RUN addgroup -S appuser && adduser -S -G appuser appuser
+USER appuser
+
+ENTRYPOINT [ "python" ]
+
+CMD [ "app.py" ]
---
+++ app.py
@@ -0,0 +1,93 @@
+import requests
+from flask import Flask, request, Response
+import logging
+import json
+import sys
+import socket
+from datetime import datetime
+
+METADATA_URL = 'http://metadata.google.internal/computeMetadata/v1/'
+METADATA_HEADERS = {'Metadata-Flavor': 'Google'}
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+
+    # get project ID
+    try:
+        r = requests.get(METADATA_URL + 'project/project-id', headers=METADATA_HEADERS)
+        if r.ok:
+            project_id = r.text
+        else:
+            project_id = None
+    except:
+
+        project_id = None
+
+    # get zone 
+    try:
+        r = requests.get(METADATA_URL + 'instance/zone', headers=METADATA_HEADERS)
+        if r.ok:
+            zone = str(r.text.split("/")[3])
+        else:
+            zone = None
+    except:
+
+        zone = None 
+
+    # get hostname 
+    try:
+        r = requests.get(METADATA_URL + 'instance/hostname', headers=METADATA_HEADERS)
+        if r.ok:
+            host_name = str(r.text)
+        else:
+            host_name = None
+    except:
+
+        host_name = None
+
+    # get cluster 
+    try:
+        r = requests.get(METADATA_URL + 'instance/attributes/cluster-name', headers=METADATA_HEADERS)
+        if r.ok:
+            cluster_name = str(r.text)
+        else:
+            cluster_name = None
+    except:
+
+        cluster_name = None
+
+    # get pod name
+    pod_name = socket.gethostname()
+
+    # get datetime
+    timestamp=datetime.now().replace(microsecond=0).isoformat()
+
+    payload = {}
+    payload['project_id'] = project_id
+    payload['pod_name'] = pod_name
+    payload['timestamp'] = timestamp
+    payload['host_name'] = host_name
+    payload['zone'] = zone
+    payload['cluster_name'] = cluster_name
+
+    return json.dumps(payload)
+
+@app.route('/healthz')
+def i_am_healthy():
+    return ('OK')
+
+
+if __name__ == '__main__':
+    out_hdlr = logging.StreamHandler(sys.stdout)
+    fmt = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    out_hdlr.setFormatter(fmt)
+    out_hdlr.setLevel(logging.INFO)
+    logging.getLogger().addHandler(out_hdlr)
+    logging.getLogger().setLevel(logging.INFO)
+    app.logger.handlers = []
+    app.logger.propagate = True
+    app.run(host='0.0.0.0', port=5000)
+
+
